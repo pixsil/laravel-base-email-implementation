fd6b00595602b6375244d4c7915f6acd6aa4ed96
pixsil <57232302+pixsil@users.noreply.github.com>
2021-04-14 14:05:35 +0200

    Create ExampleMessage.php

---
+++ Mail/ExampleMessage.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace App\Mail;
+
+
+use App\Traits\EmailTrait;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+
+class SiteFeedbackCreated extends Mailable
+{
+    use Queueable, SerializesModels, EmailTrait;
+
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Array $data_arr = [])
+    {
+        // set placeholders
+        $this->placeholder_arr = $this->to_placeholder_arr($data_arr);
+
+
+        // set email
+        $this->setupEmail();
+    }
+
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject($this->email->subject)->markdown('emails.base');
+    }
+}
