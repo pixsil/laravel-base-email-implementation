794995d24de260a64087491c870838bcbbeaf5bd
pixsil <57232302+pixsil@users.noreply.github.com>
2021-04-14 14:30:19 +0200

    Update EmailTrait.php

--- Traits/EmailTrait.php
+++ Traits/EmailTrait.php
@@ -1,1 +1,91 @@
+<?php
 
+
+// version 3
+
+
+namespace App\Traits;
+
+
+use App\Email;
+
+
+trait EmailTrait
+{
+    public $email;
+    private $placeholder_arr;
+
+
+    /**
+     * get storage path
+     */
+    public function setupEmail()
+    {
+        // guard always extent
+        if (static::class == 'App\Mail\BaseMailable') {
+            return abort(403);
+        }
+
+
+        // get the body text
+        $this->email = Email::firstOrCreate(
+            ['mailable' => class_basename(static::class)],
+            [
+                'subject' => class_basename(static::class),
+                'text' => implode(', ', array_keys($this->placeholder_arr ?? []))
+            ]
+        );
+
+
+        // replace placeholders
+        if ($this->placeholder_arr) {
+            $this->email->text = $this->replace_placeholders($this->email->text);
+        }
+    }
+
+
+    /**
+     * get storage path
+     */
+    public function to_placeholder_arr($data_arr)
+    {
+        // get the body text
+        foreach ($data_arr as $key => $data) {
+
+
+            // write new
+            $data_arr['{{'. $key .'}}'] = $data;
+
+
+            // unset old
+            unset($data_arr[$key]);
+        }
+
+
+        return $data_arr;
+    }
+
+
+    /**
+     * get storage path
+     */
+    public function replace_placeholders($text)
+    {
+        // get the body text
+        $text = strtr($text, $this->placeholder_arr);
+
+
+        return $text;
+    }
+}
+
+
+/*
+        // set placeholders
+        $this->placeholder_arr = [
+        ];
+
+
+        // set email
+        $this->setEmail();
+*/
