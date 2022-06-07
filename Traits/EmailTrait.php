3e8308c438c61f547a0e673b51be6eb0996ce8c8
pixsil <57232302+pixsil@users.noreply.github.com>
2022-06-07 14:30:47 +0200

    Update EmailTrait.php

--- Traits/EmailTrait.php
+++ Traits/EmailTrait.php
@@ -1,6 +1,6 @@
 <?php
 
-// version 4
+// version 5
 
 namespace App\Traits;
 
@@ -9,50 +9,73 @@ use App\Models\Email;
 trait EmailTrait
 {
     public $email;
-    public $placeholderData = [];
+    public $placeholder_data = [];
     private $template;
+    private $object_arr = [];
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($object_arr)
+    {
+        // set email
+        $this->setupEmail($object_arr);
+    }
 
     /**
      * get storage path
      */
-    public function setupEmail($compactObjectArr)
+    public function setupEmail($object_arr)
     {
         // guard always extent
         if (static::class == 'App\Mail\BaseMailable') {
             abort(403);
         }
 
-        // install objects
-        foreach ($compactObjectArr as $name => $object) {
-            $this->$name = $object;
+        // install objects so you can use them inside the email
+        foreach ($object_arr as $key => $object) {
+
+            // if a specific key is set use this as name
+            $object_name = is_string($key) ? $key : $object->getTable();
+
+            // set as object
+            $this->$object_name = $object;
+
+            // also fill an array with all the possible markers
+            foreach ($object->getAttributes() as $column => $attribute) {
+                $this->placeholder_data['{{'. $object_name .'.'. $column .'}}'] = $attribute;
+            }
         }
 
-        // install placeholders
-        foreach ($this->placeholders as $placeholderName => $placeholderValue) {
-            list($object, $key) = explode('.',$placeholderValue);
-            $this->placeholderData[$placeholderName] = $this->$object->$key;
-        }
+        // // install placeholders
+        // foreach ($this->placeholders as $placeholderName => $placeholderValue) {
+        //     list($object, $key) = explode('.',$placeholderValue);
+        //     $this->placeholder_data[$placeholderName] = $this->$object->$key;
+        // }
 
         // get the body text
         $this->email = Email::firstOrCreate(
             ['mailable' => class_basename(static::class)],
             [
                 'subject' => class_basename(static::class),
-                'text' => implode(', ', array_keys($this->placeholder_arr ?? [])),
+                'text' => implode(', ', array_keys($this->placeholder_data ?? [])),
                 'template' => 'emails.base',
             ]
         );
 
         // if we have placeholders to replace
-        if ($this->placeholderData) {
+        if ($this->placeholder_data) {
+            $this->email->subject = $this->replace_placeholders($this->email->subject);
             $this->email->text = $this->replace_placeholders($this->email->text);
         }
     }
 
     /**
-     * get storage path
+     *
      */
-    public function to_placeholder_arr($data_arr)
+    public function merge_to_placeholder_arr($data_arr)
     {
         // get the body text
         foreach ($data_arr as $key => $data) {
@@ -73,7 +96,7 @@ trait EmailTrait
     public function replace_placeholders($text)
     {
         // get the body text
-        $text = strtr($text, $this->placeholderData);
+        $text = strtr($text, $this->placeholder_data);
 
         return $text;
     }
@@ -100,11 +123,3 @@ trait EmailTrait
         return $this->subject($this->email->subject)->markdown($this->template);
     }
 }
-
-    /*
-            // set placeholders
-            $this->placeholder_arr = [
-            ];
-            // set email
-            $this->setEmail();
-    */
