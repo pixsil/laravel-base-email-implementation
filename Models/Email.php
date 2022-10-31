43ae96361c2e6736dfdc8a179ab54c5f9f2e4369
pixsil <57232302+pixsil@users.noreply.github.com>
2022-10-31 13:37:59 +0100

    Update Email.php

--- Models/Email.php
+++ Models/Email.php
@@ -9,5 +9,5 @@ use Illuminate\Database\Eloquent\Model;
 class Email extends Model
 {
     //
-    public $fillable = ['identifier', 'mailable', 'subject', 'text', 'template'];
+    protected $fillable = ['identifier', 'mailable', 'subject', 'text', 'template'];
 }
