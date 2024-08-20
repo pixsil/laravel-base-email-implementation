86c01d77a3028c4d2cf44b5d54d3ae1ad2d2a601
pixsil <57232302+pixsil@users.noreply.github.com>
2024-08-20 19:55:03 +0200

    Update general.blade.php

--- Views/general.blade.php
+++ Views/general.blade.php
@@ -2,6 +2,16 @@
 
 {!! $email->text !!}
 
+@if(isset($button))
+
+<x-mail::button :url="$button['url']">
+
+{{ $button['text'] }}
+
+</x-mail::button>
+
+@endif
+
 Thanks,<br>
 {{ config('app.name') }}
 </x-mail::message>
